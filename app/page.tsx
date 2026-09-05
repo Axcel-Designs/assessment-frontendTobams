31561e79273f07a08f456da767917b9009e7c937
excel <axceldesigns@gmail.com>
2026-09-05 11:22:26 +0100

    feat: components added

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,23 @@
+import Consultant from "@/components/Consultant";
+import BookConsultation from "@/components/BookConsultation";
+import Hero from "@/components/Hero";
+import LearningManagementSystem from "@/components/LearningManagementSystem";
+import ManagementDevelopmentProgram from "@/components/ManagementDevelopmentProgram";
+import Testimonials from "@/components/Testimonials";
+import Training from "@/components/Training";
+import TransformationHub from "@/components/TransformationHub";
 
 export default function Home() {
   return (
-    <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white">
-    
-    </main>
+    <div className="flex w-full flex-col justify-between">
+      <Hero/>
+      <LearningManagementSystem/>
+      <Training/>
+      <ManagementDevelopmentProgram/>
+      <TransformationHub/>
+      <Consultant/>
+      <BookConsultation/>
+      <Testimonials/>
+    </div>
   );
 }
