da9019d8e6f5b8f139a7536570de114367becf59
excel <axceldesigns@gmail.com>
2026-09-05 02:52:26 +0100

    feat: add testimonial,footer and training data

---
+++ data/data.ts
@@ -0,0 +1,107 @@
+import { assetImg } from "@/public/images/imgs";
+
+export const testimonialData = [
+  {
+    img: assetImg.aishaYusuf,
+    name: "Aisha Yusuf",
+    title: "Founder, CraftHub NG",
+    message:
+      "Working with Tobams Group on our website was a breeze. They understood our vision and transformed it into a beautiful online space. Highly recommend their Website Design service!",
+  },
+  {
+    img: assetImg.johnDavies,
+    name: "John Davies",
+    title: "Marketing Manager, E-Commerce Emporium",
+    message:
+      "Tobams Group's Digital Marketing strategies gave our brand the boost it needed. Simple yet powerful techniques that delivered tangible results. A pleasure to collaborate with!",
+  },
+  {
+    img: assetImg.chinoso,
+    name: "Chinonso Nwankwo",
+    title: "HR Director, FutureTech Solutions",
+    message:
+      "Tobams Group has been instrumental in our talent acquisition journey. Their Tech Talent Solution service consistently connects us with the right professionals. Reliable and straightforward.",
+  },
+  {
+    img: "",
+    name: "",
+    title: "",
+    message: "",
+  },
+];
+
+export const trainingData = [
+  {
+    img: assetImg.corporateTraining,
+    title: "Corporate Trainings ",
+    desc: "Empower your team with our customised Corporate Training programs designed to address the unique needs and objectives of your organisation. Our expert facilitators work closely with your team to deliver tailored learning experiences that align with your company's goals and values.",
+    lists: [
+      "Leadership Training",
+      "Strategic Planning and Implementation",
+      "Project Management",
+      "Sustainability Training",
+      "Customised Training",
+    ],
+  },
+  {
+    img: assetImg.pit,
+    title: "Personalised Individual Training",
+    desc: "Begin a journey of lifelong learning and professional development with Tobams Group's diverse range of training programs for individuals. From technical skills mastery to soft skills enhancement, our courses cover a wide spectrum of topics to meet the evolving needs of today's professionals.  ",
+    lists: [
+      "Leadership Development",
+      "Soft Skills Development",
+      "Industry Specific Knowledge",
+      "Technical Skills Enhancement",
+      "Time Management and Productivity",
+      "Career Development",
+    ],
+  },
+  {
+    img: assetImg.capacityDev,
+    title: "Capacity Development",
+    desc: "At Tobams Group, we empower individuals and organizations through tailored training programs, expert-led workshops, and personalized mentorship. We are committed to your success and growth. We are dedicated to providing a comprehensive suite of benefits designed to foster your development and success:",
+    lists: [
+      "Tailored Training Programs",
+      "Expert-Led Workshops",
+      "Personalized Mentorship",
+      "Technical Skills Enhancement",
+      "Collaborative Learning Environment",
+      "Ongoing Support and Resources",
+    ],
+  },
+];
+export const WhatWeDoData = [
+  "Sustainability Services",
+  "Strategy Planning and Implementation",
+  "Tech Talent Solutions",
+  "Training and Development",
+  "IT Consulting Services ",
+  "Social Impact",
+  "Talent Recruitment",
+];
+
+export const companyData = [
+  { title: "About", link: "/about" },
+  { title: "Jobs", link: "/jobs" },
+  { title: "Projects", link: "/projects" },
+  { title: "Our Founders", link: "/our-founders" },
+  { title: "Business Model", link: "/business-model" },
+  { title: "The Team", link: "/the-team" },
+  { title: "Contact Us", link: "/contact-us" },
+  { title: "Blog", link: "/blog" },
+  { title: "FAQs", link: "/faqs" },
+  {
+    title: "Testimonials",
+    link: "/testimonials                                                                                         ",
+  },
+];
+
+export const solutionsData = [
+  { title: "Tobams Group Academy", link: "/academy" },
+  { title: "Help a Tech Talent", link: "/help-a-tech-talent" },
+  { title: "Campus Ambassadors Program", link: "/campus-ambassadors-program" },
+  { title: "Join Our Platform", link: "/join-our-platform" },
+  { title: "Pricing", link: "/pricing" },
+  { title: "Book a Consultation", link: "/book-a-consultation" },
+  { title: "Join Our Slack Community", link: "/join-our-slack-community" },
+];
